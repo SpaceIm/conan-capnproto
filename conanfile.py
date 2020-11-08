db277e7a05bf7923139668f7416eee311b6f7cce
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-11-08 01:38:44 +0100

    wip

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,8 @@
+from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
+from conans.errors import ConanInvalidConfiguration
+import glob
 import os
 
-from conans import ConanFile, AutoToolsBuildEnvironment, CMake, tools
-from conans.errors import ConanInvalidConfiguration
 
 class CapnprotoConan(ConanFile):
     name = "capnproto"
@@ -10,26 +11,24 @@ class CapnprotoConan(ConanFile):
     topics = ("conan", "capnproto", "serialization", "rpc")
     homepage = "https://capnproto.org"
     url = "https://github.com/conan-io/conan-center-index"
-    exports_sources = "CMakeLists.txt"
+    exports_sources = ("CMakeLists.txt", "patches/*")
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "lite": [True, False],
         "with_openssl": [True, False],
         "with_zlib": [True, False]
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "lite": False,
         "with_openssl": True,
         "with_zlib": True
     }
 
-    _autotools = None
     _cmake = None
+    _autotools = None
 
     @property
     def _source_subfolder(self):
@@ -51,8 +50,11 @@ class CapnprotoConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+            del self.options.openssl
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, 14)
         mininum_compiler_version = self._minimum_compilers_version.get(str(self.settings.compiler))
@@ -60,47 +62,20 @@ class CapnprotoConan(ConanFile):
             raise ConanInvalidConfiguration("Cap'n Proto doesn't support {0} {1}".format(self.settings.compiler, self.settings.compiler.version))
         if self.settings.compiler == "Visual Studio" and self.options.shared:
             raise ConanInvalidConfiguration("Cap'n Proto doesn't support shared libraries for Visual Studio")
-        if self.options.lite:
-            del self.options.with_openssl
-            del self.options.with_zlib
 
     def requirements(self):
         if self.options.get_safe("with_openssl"):
-            self.requires("openssl/1.1.1g")
-        if self.options.get_safe("with_zlib"):
+            self.requires("openssl/1.1.1h")
+        if self.options.with_zlib:
             self.requires("zlib/1.2.11")
+
+    def build_requirements(self):
+        if self.settings.os != "Windows":
+            self.build_requires("autoconf/2.69")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        os.rename("capnproto-c++-" + self.version, self._source_subfolder)
-
-    def build(self):
-        if self._use_autotools:
-            autotools = self._configure_autotools()
-            autotools.make()
-        else:
-            cmake = self._configure_cmake()
-            cmake.build()
-
-    @property
-    def _use_autotools(self):
-        return self.settings.os != "Windows"
-
-    def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self)
-        args = [
-            "--enable-shared" if self.options.shared else "--disable-shared",
-            "--disable-static" if self.options.shared else "--enable-static",
-            "--disable-reflection" if self.options.lite else "--enable-reflection",
-            "--with-openssl" if self.options.get_safe("with_openssl") else "--without-openssl",
-            "--with-zlib" if self.options.get_safe("with_zlib") else "--without-zlib"
-        ]
-        if self.options.lite:
-            args.append("--with-external-capnp")
-        self._autotools.configure(configure_dir=self._source_subfolder, args=args)
-        return self._autotools
+        os.rename(self.name + "-" + self.version, self._source_subfolder)
 
     def _configure_cmake(self):
         if self._cmake:
@@ -108,41 +83,118 @@ class CapnprotoConan(ConanFile):
         self._cmake = CMake(self)
         self._cmake.definitions["BUILD_TESTING"] = False
         self._cmake.definitions["EXTERNAL_CAPNP"] = False
-        self._cmake.definitions["CAPNP_LITE"] = self.options.lite
+        self._cmake.definitions["CAPNP_LITE"] = False
         self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
+    def _configure_autotools(self):
+        if self._autotools:
+            return self._autotools
+        args = []
+        if self.options.shared:
+            args.extend(["--disable-static", "--enable-shared"])
+        else:
+            args.extend(["--disable-shared", "--enable-static"])
+        args.append("--with-openssl" if self.options.with_openssl else "--without-openssl")
+        args.append("--with-zlib" if self.options.with_zlib else "--without-zlib")
+        args.append("--enable-reflection")
+        self._autotools = AutoToolsBuildEnvironment(self)
+        self._autotools.configure(args=args, configure_dir=os.path.join(self._source_subfolder, "c++"))
+        return self._autotools
+
+    def build(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+        if self.settings.os == "Windows":
+            cmake = self._configure_cmake()
+            cmake.build()
+        else:
+            with tools.chdir(os.path.join(self._source_subfolder, "c++")):
+                self.run("{} --install --verbose -Wall".format(tools.get_env("AUTORECONF")))
+            autotools = self._configure_autotools()
+            autotools.make()
+
+    @property
+    def _cmake_folder(self):
+        return os.path.join("lib", "cmake", "CapnProto")
+
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
-        if self._use_autotools:
-            autotools = self._configure_autotools()
-            autotools.install()
-        else:
+        if self.settings.os == "Windows":
             cmake = self._configure_cmake()
             cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        else:
+            autotools = self._configure_autotools()
+            autotools.install()
+            for la_file in glob.glob(os.path.join(self.package_folder, "lib", "*.la")):
+                os.remove(la_file)
+        for cmake_file in glob.glob(os.path.join(self.package_folder, self._cmake_folder, "*")):
+            if os.path.basename(cmake_file) not in ["CapnProtoMacros.cmake", "CapnProtoTargets.cmake"]:
+                os.remove(cmake_file)
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
-        self.cpp_info.libs = self._get_ordered_libs()
-        if self.options.lite:
-            self.cpp_info.defines.append("CAPNP_LITE=1")
+        self.cpp_info.names["cmake_find_package"] = "CapnProto"
+        self.cpp_info.names["cmake_find_package_multi"] = "CapnProto"
+        # capnp
+        self.cpp_info.components["capnp"].names["cmake_find_package"] = "capnp"
+        self.cpp_info.components["capnp"].names["cmake_find_package_multi"] = "capnp"
+        self.cpp_info.components["capnp"].names["pkg_config"] = "capnp"
+        self.cpp_info.components["capnp"].libs = ["capnp"]
+        self.cpp_info.components["capnp"].requires = ["kj"]
+        # capnp-json
+        self.cpp_info.components["capnp-json"].names["cmake_find_package"] = "capnp-json"
+        self.cpp_info.components["capnp-json"].names["cmake_find_package_multi"] = "capnp-json"
+        self.cpp_info.components["capnp-json"].names["pkg_config"] = "capnp-json"
+        self.cpp_info.components["capnp-json"].libs = ["capnp-json"]
+        self.cpp_info.components["capnp-json"].requires = ["capnp", "kj"]
+        # capnp-rpc
+        self.cpp_info.components["capnp-rpc"].names["cmake_find_package"] = "capnp-rpc"
+        self.cpp_info.components["capnp-rpc"].names["cmake_find_package_multi"] = "capnp-rpc"
+        self.cpp_info.components["capnp-rpc"].names["pkg_config"] = "capnp-rpc"
+        self.cpp_info.components["capnp-rpc"].libs = ["capnp-rpc"]
+        self.cpp_info.components["capnp-rpc"].requires = ["capnp", "kj", "kj-async"]
+        # kj
+        self.cpp_info.components["kj"].names["cmake_find_package"] = "kj"
+        self.cpp_info.components["kj"].names["cmake_find_package_multi"] = "kj"
+        self.cpp_info.components["kj"].names["pkg_config"] = "kj"
+        self.cpp_info.components["kj"].libs = ["kj"]
         if self.settings.os == "Linux":
-            self.cpp_info.system_libs.append("pthread")
+            self.cpp_info.components["kj"].system_libs = ["pthread"]
+        self.cpp_info.components["kj"].builddirs = self._cmake_folder
+        self.cpp_info.components["kj"].build_modules = [os.path.join(self._cmake_folder, "CapnProtoMacros.cmake"),
+                                                        os.path.join(self._cmake_folder, "CapnProtoTargets.cmake")]
+        # kj-async
+        self.cpp_info.components["kj-async"].names["cmake_find_package"] = "kj-async"
+        self.cpp_info.components["kj-async"].names["cmake_find_package_multi"] = "kj-async"
+        self.cpp_info.components["kj-async"].names["pkg_config"] = "kj-async"
+        self.cpp_info.components["kj-async"].libs = ["kj-async"]
+        if self.settings.os == "Linux":
+            self.cpp_info.components["kj-async"].system_libs = ["pthread"]
         elif self.settings.os == "Windows":
-            self.cpp_info.system_libs.append("ws2_32")
-        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
+            self.cpp_info.components["kj-async"].system_libs = ["ws2_32"]
+        self.cpp_info.components["kj-async"].requires = ["kj"]
+        # kj-http
+        self.cpp_info.components["kj-http"].names["cmake_find_package"] = "kj-http"
+        self.cpp_info.components["kj-http"].names["cmake_find_package_multi"] = "kj-http"
+        self.cpp_info.components["kj-http"].names["pkg_config"] = "kj-http"
+        self.cpp_info.components["kj-http"].libs = ["kj-http"]
+        self.cpp_info.components["kj-http"].requires = ["kj", "kj-async"]
+        # kj-gzip
+        if self.options.with_zlib:
+            self.cpp_info.components["kj-gzip"].names["cmake_find_package"] = "kj-gzip"
+            self.cpp_info.components["kj-gzip"].names["cmake_find_package_multi"] = "kj-gzip"
+            self.cpp_info.components["kj-gzip"].names["pkg_config"] = "kj-gzip"
+            self.cpp_info.components["kj-gzip"].libs = ["kj-gzip"]
+            self.cpp_info.components["kj-gzip"].requires = ["kj", "kj-async", "zlib::zlib"]
+        # kj-tls
+        if self.options.get_safe("with_openssl"):
+            self.cpp_info.components["kj-tls"].names["cmake_find_package"] = "kj-tls"
+            self.cpp_info.components["kj-tls"].names["cmake_find_package_multi"] = "kj-tls"
+            self.cpp_info.components["kj-tls"].names["pkg_config"] = "kj-tls"
+            self.cpp_info.components["kj-tls"].libs = ["kj-tls"]
+            self.cpp_info.components["kj-tls"].requires = ["kj", "kj-async", "openssl::openssl"]
 
-    def _get_ordered_libs(self):
-        libs = []
-        if not self.options.lite:
-            # kj-async is a dependency of kj-tls, kj-gzip, capnp-rpc, capnp-json and kj-http
-            if self.options.with_openssl:
-                libs.append("kj-tls")
-            if self.options.with_zlib:
-                libs.append("kj-gzip")
-            libs.extend(["capnp-rpc", "capnp-json", "kj-http", "kj-async", "capnpc"])
-        # - capnp is a dependency of capnp-rpc and capnp-json
-        # - kj is a dependency of kj-tls, kj-gzip, capnp-rpc, capnp-json, kj-http, kj-async, capnpc and capnp
-        libs.extend(["capnp", "kj"])
-        return libs
+        bin_path = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH env var with: {}".format(bin_path))
+        self.env_info.PATH.append(bin_path)
